136674524a77a15a25d0be8a2d75e56204103975
Xaraf89 <alex.mastro.1989@gmail.com>
2020-03-16 23:07:27 +0100

    Update

--- array/grades.js
+++ array/grades.js
@@ -1,1 +1,46 @@
+//creare un oggetto con proprietà nome cognome e la chiamata voto e visualizza a schermo la media
+/*
+let voti = [5, 6, 9, 7, 3, 6];
+const nome = ["Mario"];
+const cognome = ["Rossi"];
+
+var votiEx1, votiEx2
+votiEx1 = prompt("che voto vuoi aggiungere");
+votiEx2 = prompt("che voto vuoi aggiungere");
+voti.push(Number(votiEx1), Number(votiEx2))
+
+let count = voti.length;
+voti = voti.reduce((previous, current) => current += previous);
+voti /= count;
+console.log(nome + cognome + " ha la seguente media " + voti)
+*/
+
+const studente = {
+    chimica: [],
+    vulcanologia: [4, 6, 9, 1, 1, 1, 11, 11, 1, 1, 1, 1, 1,],
+    latino: [],
+    astrologia: []
+}
+//const ch = "chimica"
+//console.log(studente.chimica)
+//console.log(studente[ch])
+
+function aggiungiVoto(materia, voto) {
+    studente[materia].push(voto)
+}
+
+aggiungiVoto("chimica", 5)
+aggiungiVoto("chimica", 7)
+console.log(studente)
+
+function mediaVoto(materia) {
+    return studente[materia].reduce((ac, cv) => ac + cv) / studente[materia].length
+}
+
+console.log(mediaVoto("vulcanologia"))
+
+
+
+
+
 
--- array/letters.js
+++ array/letters.js
@@ -1,1 +1,4 @@
-
+let array = ["ape", 2, 3, 4, 5, 6, 7, 8, 9]
+let array2 = [...array].reverse();
+console.log(array2)
+console.log(array)
