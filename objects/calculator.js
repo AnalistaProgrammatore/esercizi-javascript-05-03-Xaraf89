2dbca0066845ff4443497819d8b249aa877056ee
Xaraf89 <alex.mastro.1989@gmail.com>
2020-03-23 11:11:58 +0100

    Update

--- objects/calculator.js
+++ objects/calculator.js
@@ -1,1 +1,25 @@
+/*creare nel file calculator.js un oggetto calculator con 
+una proprietà values: un array di valori e 4 metodi add, sub, mul, div che 
+eseguano le 4 operazioni fondamentali sulla proprietà values e restituiscano 
+in output il risultato dell'operazione. Ad esempio: */
 
+let calculator = {
+    sum: function () {
+        return this.values.reduce((x, y) => x + y)
+    },
+    sub: function () {
+        return this.values.reduce((x, y) => x - y)
+    },
+    mul: function () {
+        return this.values.reduce((x, y) => x * y)
+    },
+    div: function () {
+        return this.values.reduce((x, y) => x / y)
+    }
+}
+
+calculator.values = [4, 5, 6, 0];
+console.log(calculator.sum())
+console.log(calculator.sub())
+console.log(calculator.mul())
+console.log(calculator.div())
--- objects/multiply.js
+++ objects/multiply.js
@@ -1,1 +1,25 @@
+//Scrivi nel file multiply.js una funzione multiply che dato in input un oggetto 
+//e un numero moltiplichi tutte le proprietà numeriche dell'oggetto per il numero 
+//dato. NB la funzione deve essere pura. Ad esempio
 
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+}
+//const newMenu = multiplyNumeric(menu, 2)
+////////////////////////////////////////////////////
+
+//versione light
+
+console.log([menu.width * 2, menu.height * 2, menu.title])
+
+//versione funzione
+var newMenu = [];
+
+menu.forEach((menu) =>
+    newMenu.push(menu.width * 2),
+    newMenu.push(menu.height * 2),
+    newMenu.push(menu.title)
+);
+console.log(newMenu());
